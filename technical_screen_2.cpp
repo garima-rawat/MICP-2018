f0583dd0f43e9b5585cafa66db5671eb9d94410f
Garima Rawat <32599086+garima-rawat@users.noreply.github.com>
2018-02-09 13:40:59 +0530

    Create technical_screen_2.cpp

---
+++ technical_screen_2.cpp
@@ -0,0 +1,25 @@
+/*
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+*/
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num/10==0)
+            return num;
+        int n=0;
+        while(num>0)
+        {
+            n=n+num%10;
+            num=num/10;
+        }
+        return(addDigits(n));
+        
+        
+    }
+};
